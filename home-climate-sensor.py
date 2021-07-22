aeaa0396202073ddd8b2ecc2163fa082b4b05ec5
Andrew Krueger <akrueger@adjacent-tech.com>
2021-07-22 00:25:21 -0500

    Initial draft

---
+++ home-climate-sensor.py
@@ -0,0 +1,26 @@
+import time
+import math
+import board
+from adafruit_bme280 import basic as adafruit_bme280
+
+# Uses board.SCL and board.SDA
+i2c = board.IC2()
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+temp = bme280.temperature
+humid = bme280.humidity
+pressure = bme280.pressure
+
+# Magnus formula
+b = 17.62
+c = 243.12
+gamma = (b * temp / (c + temp)) + math.log(humid / 100)
+dew_point = (c * gamma) / (b - gamma)
+
+# Sensor needs a moment to gather initial readings
+time.sleep(1)
+
+print("\nTemperature: %0.1f C % temp")
+print("\Humidity: %0.1f %% humid")
+print("\nDew Point: %0.1f %% dew_point")
+print("\nPressure: %0.1f hPa % pressure")
