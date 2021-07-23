a9089f481375370a1233578bdcef87022ff1185e
Andrew Krueger <akrueger@adjacent-tech.com>
2021-07-22 20:43:39 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # home-climate-sensor
 BME280 sensor array
+
+Enable I2C I/O
+sudo raspi-config
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get i2c-tools
+sudo pip3 install adafruit-circuitpython-bme280
+sudo pip3 install ISStreamer
--- home-climate-sensor.py
+++ home-climate-sensor.py
@@ -2,25 +2,49 @@ import time
 import math
 import board
 from adafruit_bme280 import basic as adafruit_bme280
+from ISStreamer.Streamer import Streamer
+
+# --------- Settings ---------
+SENSOR_NAME = "[NAME: SENSOR #]"
+BUCKET_NAME = "[NAME]"
+BUCKET_KEY = "[KEY]"
+ACCESS_KEY = "[KEY]"
+MINUTES_BETWEEN_READS = 5
+METRIC_UNITS = False
+# ---------------------------------
 
 # Uses board.SCL and board.SDA
 i2c = board.I2C()
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
-temp = bme280.temperature
-humid = bme280.humidity
-pressure = bme280.pressure
-
-# Magnus formula
-b = 17.62
-c = 243.12
-gamma = (b * temp / (c + temp)) + math.log(humid / 100)
-dew_point = (c * gamma) / (b - gamma)
+streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
 # Sensor needs a moment to gather initial readings
 time.sleep(1)
 
-print("\nTemperature: %0.1f C" % temp)
-print("\nHumidity: %0.1f %%" % humid)
-print("\nDew Point: %0.1f %%" % dew_point)
-print("\nPressure: %0.1f hPa" % pressure)
+while True:
+    try:
+        temperature_c = bme280.temperature
+        humidity = bme280.humidity
+        pressure = bme280.pressure
+
+        # Magnus formula for dew point
+        b = 17.62
+        c = 243.12
+        gamma = (b * temperature_c / (c + temperature_c)) + math.log(humidity / 100)
+        dew_point = (c * gamma) / (b - gamma)
+
+    except RuntimeError:
+        print("RuntimeError, trying again...")
+        continue
+                
+    if METRIC_UNITS:
+        streamer.log(SENSOR_NAME + " Temperature(C)", temperature_c)
+    else:
+        temperature_f = format(temperature_c * 9.0 / 5.0 + 32.0, ".2f")
+        streamer.log(SENSOR_NAME + " Temperature(F)", temperature_f)
+        humidity = format(humidity,".2f")
+        streamer.log(SENSOR_NAME + " Humidity(%)", humidity)
+        streamer.log(SENSOR_NAME + " Dew Point(%)", dew_point)
+        streamer.flush()
+        time.sleep(60 * MINUTES_BETWEEN_READS)
