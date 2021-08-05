0ffd741540fe06ba4681353e6a628a335ac3af33
Andrew Krueger <akrueger@adjacent-tech.com>
2021-08-04 19:22:20 -0500

    Add new sensors

---
+++ aht20/aht20.py
@@ -0,0 +1,55 @@
+import time
+import math
+import os
+import board
+import adafruit_ahtx0
+from ISStreamer.Streamer import Streamer
+
+# --------- Settings ---------
+DEVICE_NAME = os.environ.get("BALENA_DEVICE_NAME_AT_INIT")
+BUCKET_NAME = os.environ.get("INITIAL_STATE_BUCKET_NAME")
+BUCKET_KEY = os.environ.get("INITIAL_STATE_BUCKET_KEY")
+ACCESS_KEY = os.environ.get("INITIAL_STATE_ACCESS_KEY")
+MINUTES_BETWEEN_READS = float(os.environ.get("MINUTES_BETWEEN_READS", 5))
+USE_METRIC = os.environ.get("USE_METRIC")
+TEMPERATURE_OFFSET = float(os.environ.get("TEMPERATURE_OFFSET", 0))
+HUMIDITY_OFFSET = float(os.environ.get("HUMIDITY_OFFSET", 0))
+# ---------------------------------
+
+# Uses board.SCL and board.SDA
+i2c = board.I2C()
+aht20 = adafruit_ahtx0.AHTx0(i2c)
+
+streamer = Streamer(
+    bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY
+)
+
+# Sensor needs a moment to gather initial readings
+time.sleep(1)
+
+while True:
+    try:
+        temperature_c = aht20.temperature + TEMPERATURE_OFFSET
+        humidity = aht20.relative_humidity + HUMIDITY_OFFSET
+
+        # Magnus formula for dew point
+        b = 17.62
+        c = 243.12
+        gamma = (b * temperature_c / (c + temperature_c)) + math.log(humidity / 100)
+        dew_point = (c * gamma) / (b - gamma)
+
+    except RuntimeError:
+        print("RuntimeError, trying again...")
+        continue
+
+    if USE_METRIC.lower() == "true":
+        streamer.log(DEVICE_NAME + " Temperature(C)", temperature_c)
+    else:
+        temperature_f = format(temperature_c * 9.0 / 5.0 + 32.0, ".2f")
+        streamer.log(DEVICE_NAME + " Temperature(F)", temperature_f)
+
+    humidity = format(humidity, ".2f")
+    streamer.log(DEVICE_NAME + " Humidity(%)", humidity)
+    # streamer.log(DEVICE_NAME + " Dew Point(%)", dew_point)
+    streamer.flush()
+    time.sleep(60 * MINUTES_BETWEEN_READS)
--- tmp117/tmp117.py
+++ tmp117/tmp117.py
@@ -2,7 +2,7 @@ import time
 import math
 import os
 import board
-from adafruit_bme280 import basic as adafruit_bme280
+import adafruit_tmp117
 from ISStreamer.Streamer import Streamer
 
 # --------- Settings ---------
@@ -10,13 +10,14 @@ DEVICE_NAME = os.environ.get("BALENA_DEVICE_NAME_AT_INIT")
 BUCKET_NAME = os.environ.get("INITIAL_STATE_BUCKET_NAME")
 BUCKET_KEY = os.environ.get("INITIAL_STATE_BUCKET_KEY")
 ACCESS_KEY = os.environ.get("INITIAL_STATE_ACCESS_KEY")
-MINUTES_BETWEEN_READS = 5
-METRIC_UNITS = False
+MINUTES_BETWEEN_READS = float(os.environ.get("MINUTES_BETWEEN_READS", 5))
+USE_METRIC = os.environ.get("USE_METRIC")
+TEMPERATURE_OFFSET = float(os.environ.get("TEMPERATURE_OFFSET", 0))
 # ---------------------------------
 
 # Uses board.SCL and board.SDA
 i2c = board.I2C()
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+tmp117 = adafruit_tmp117.TMP117(i2c)
 
 streamer = Streamer(
     bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY
@@ -27,27 +28,17 @@ time.sleep(1)
 
 while True:
     try:
-        temperature_c = bme280.temperature
-        humidity = bme280.humidity
-        pressure = bme280.pressure
-
-        # Magnus formula for dew point
-        b = 17.62
-        c = 243.12
-        gamma = (b * temperature_c / (c + temperature_c)) + math.log(humidity / 100)
-        dew_point = (c * gamma) / (b - gamma)
+        temperature_c = tmp117.temperature + TEMPERATURE_OFFSET
 
     except RuntimeError:
         print("RuntimeError, trying again...")
         continue
 
-    if METRIC_UNITS:
+    if USE_METRIC.lower() == "true":
         streamer.log(DEVICE_NAME + " Temperature(C)", temperature_c)
     else:
         temperature_f = format(temperature_c * 9.0 / 5.0 + 32.0, ".2f")
         streamer.log(DEVICE_NAME + " Temperature(F)", temperature_f)
-        humidity = format(humidity, ".2f")
-        streamer.log(DEVICE_NAME + " Humidity(%)", humidity)
-        # streamer.log(DEVICE_NAME + " Dew Point(%)", dew_point)
-        streamer.flush()
-        time.sleep(60 * MINUTES_BETWEEN_READS)
+
+    streamer.flush()
+    time.sleep(60 * MINUTES_BETWEEN_READS)
